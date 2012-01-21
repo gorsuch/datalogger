18cffb5e4e32d8f95508c02023f1dbcfeaecf782
Michael Gorsuch <michael.gorsuch@gmail.com>
2012-01-21 08:13:39 -0600

    beginnings of actual tests

--- spec/logger_spec.rb
+++ spec/logger_spec.rb
@@ -4,5 +4,18 @@ require 'minitest/autorun'
 require 'datalogger'
 
 describe DataLogger::Logger do
-  it "must have some tests ;-)"
+  it "must respond to component" do
+    l = DataLogger::Logger.new
+    l.respond_to?(:component).must_equal(true)
+  end
+
+  it "must set component to 'app' by default" do
+    l = DataLogger::Logger.new
+    l.component.must_equal('app')
+  end
+
+  it "must allow for component to be set" do
+    l = DataLogger::Logger.new('non-default')
+    l.component.must_equal('non-default')
+  end
 end
