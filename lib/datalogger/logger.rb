e433bb2e3847f3276f1f3f7b4dd9e3a539b2139c
Michael Gorsuch <michael.gorsuch@gmail.com>
2012-01-20 21:02:30 -0600

    beginnings of a sink interface

--- lib/datalogger/logger.rb
+++ lib/datalogger/logger.rb
@@ -1,14 +1,17 @@
 module DataLogger
   class Logger
-    attr_accessor :component
+    attr_accessor :component, :sinks
 
     def initialize(component)
       self.component = component
+      self.sinks = []
+      self.sinks << STDOUTSink.new
     end
 
     def log(data)
-      stdout = STDOUTSink.new
-      stdout.log(component, data)
+      self.sinks.each do |sink|
+        sink.log(component, data)
+      end
     end
   end
 end
